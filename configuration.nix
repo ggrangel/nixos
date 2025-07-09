4e95b2193558d07f27fed2975c2128b4833de938
Gustavo Rangel <gustavorangel91@gmail.com>
2025-07-09 15:56:26 -0300

    comments

--- configuration.nix
+++ configuration.nix
@@ -2,8 +2,9 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       /etc/nixos/hardware-configuration.nix
+      <home-manager/nixos>
     ];
 
   # Bootloader.
@@ -13,19 +14,10 @@
   networking.hostName = "pdp11"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pt_BR.UTF-8";
     LC_IDENTIFICATION = "pt_BR.UTF-8";
@@ -38,7 +30,6 @@
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
@@ -55,34 +46,20 @@
     ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     git   
     home-manager
     neovim  
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  home-manager.users.ggrangel = { pkgs, ... }: {
+    home.stateVersion = "25.05";
+  };
+  
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   networking.firewall.enable = true;
   networking.firewall.allowedTCPPorts = [22 80 443];
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -107,6 +84,7 @@
   users.users.ggrangel.shell = pkgs.zsh;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # garbage collection
   nix.gc = {
     automatic = true;
     dates = "weekly";
